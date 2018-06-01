c48b7cfa33c90f07d1b64b0176d58bd15664abc8
Lukas <lordlukas19@gmail.com>
2018-06-01 14:59:03 +0200

    Project

---
+++ Networking/Assets/MyNetworkManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager {
+
+
+    public void MyHostStart()
+    {
+        Debug.Log(Time.timeSinceLevelLoad + " starting host");
+        StartHost();
+    }
+
+    public override void OnStartHost ()
+    {
+        Debug.Log(Time.timeSinceLevelLoad + " host started");
+    }
+
+    public override void OnStartClient(NetworkClient myClient)
+    {
+        Debug.Log(Time.timeSinceLevelLoad + " client has requested");
+    }
+}
